413dc9321577f1f66dc7cce1b247fde3459a56da
Andrew Sylvester <andrewrsylvester@gmail.com>
2022-02-21 18:12:36 -0600

    Final Changes

--- Assets/Game Files/Scripts/ChemistryEventReaction.cs
+++ Assets/Game Files/Scripts/ChemistryEventReaction.cs
@@ -7,11 +7,24 @@ public class ChemistryEventReaction : MonoBehaviour
 	public ChemistryObject monitoredObject;
 	public ChemistryState GoalState;
 	public GameObject objToDestroy;
+	public bool endGame = false;
 
 	private void Update()
 	{
 		if (monitoredObject.ChemistryState == GoalState)
-			Destroy(objToDestroy);
+		{
+			if (endGame)
+				StartCoroutine(EndGame());
+			else
+				Destroy(objToDestroy);
+		}
+	}
+
+	IEnumerator EndGame()
+    {
+		monitoredObject.GetComponent<MeshRenderer>().enabled = false;
+		yield return new WaitForSeconds(5);
+		Application.Quit();
 	}
 }
 
